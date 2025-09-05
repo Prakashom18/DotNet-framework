a9de983534322b5f5eb405ce399777d82b9f564e
Prakashom18 <shahomprakash097@gmail.com>
2025-09-05 09:16:11 +0545

    Second Program

---
+++ program2.cs
@@ -0,0 +1,11 @@
+class Hello
+{
+    public static void Main()
+    {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.Console.WriteLine(c);
+        System.Console.WriteLine("Hello");
+    }
+}
