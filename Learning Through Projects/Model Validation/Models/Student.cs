6a1c0d30c9642d970508a65775d471ea5a774bd6
Prakashom18 <shahomprakash097@gmail.com>
2025-11-04 22:51:23 +0545

    Model Validation

---
+++ Learning Through Projects/Model Validation/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model_Validation.Models
+{
+    public class Student
+    {
+       [Required]
+        private string name;
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+    }
+}
