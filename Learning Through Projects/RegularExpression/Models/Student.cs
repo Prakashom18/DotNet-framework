6ba0323391248fa715d4a14cee48e2d06b67cba7
Prakashom18 <shahomprakash097@gmail.com>
2025-11-06 09:35:23 +0545

    Regular Expression

---
+++ Learning Through Projects/RegularExpression/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RegularExpression.Models
+
+{
+    public class Student
+    {
+        [Required]
+        //[RegularExpression=]
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+      
+
+    }
+}
