12854c3c12dfe4d4a3173259ae073071e208114e
Prakashom18 <shahomprakash097@gmail.com>
2025-11-15 21:46:02 +0545

    Database first

--- dotnet/CodeFirst/Models/Student.cs
+++ dotnet/CodeFirst/Models/Student.cs
@@ -1,9 +1,14 @@
-﻿namespace CodeFirst.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace CodeFirst.Models
 {
     public class Student
     {
+        [Key]
         public int Id { get; set; }
+        [Column("StudentName",TypeName ="varchar(100)")]
         public string Name { get; set; }
+
         public string Gender { get; set; }
         public int Age { get; set; }
     }
