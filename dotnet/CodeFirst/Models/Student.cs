80d9b134b831fa99a501cc234d4ae0efe379e571
Prakashom18 <shahomprakash097@gmail.com>
2025-11-16 20:45:14 +0545

    Database first

--- dotnet/CodeFirst/Models/Student.cs
+++ dotnet/CodeFirst/Models/Student.cs
@@ -8,8 +8,9 @@ namespace CodeFirst.Models
         public int Id { get; set; }
         [Column("StudentName",TypeName ="varchar(100)")]
         public string Name { get; set; }
-
+        [Column("StudentGender", TypeName = "varchar(100)")]
         public string Gender { get; set; }
+
         public int Age { get; set; }
     }
 }
