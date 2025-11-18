e268172e39cc13cb349a0e88dacec23a56b65d18
Prakashom18 <shahomprakash097@gmail.com>
2025-11-18 21:33:20 +0545

    Adding Session

---
+++ dotnet/Session/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Session.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Session.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("mykey", "Programemntor");
+
+
+            return View();
+        }
+        public IActionResult Get()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
